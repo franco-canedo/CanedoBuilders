954d69d6deedc742b9fc866d26551b6cea3d1b63
franco-canedo <francanedo1@hotmail.com>
2021-08-13 16:12:20 -0500

    gallery head picture start

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './Gallery.css';
+import house from '../photos/house.jpg';
 
 
 function Gallery() {
@@ -7,6 +8,12 @@ function Gallery() {
         <div>
             <div>
                 <h1>Gallery</h1>
+            </div>
+            <div className="head-picture-container"> 
+                <img className="head-image" src={house} alt="house"/>
+            </div>
+            <div>
+
             </div>
         </div>
 
