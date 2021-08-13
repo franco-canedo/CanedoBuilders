25a0dd020f75276d5c16415f358a7cf9b15daf32
franco-canedo <francanedo1@hotmail.com>
2021-08-13 17:42:37 -0500

    slide in menu start

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -3,16 +3,33 @@ import './Gallery.css';
 import house from '../photos/house.jpg';
 
 
+
 function Gallery() {
     return (
         <div>
             <div>
                 <h1>Gallery</h1>
             </div>
-            <div className="head-picture-container"> 
-                <img className="head-image" src={house} alt="house"/>
+            <div className="head-picture-container">
+                <img className="head-image" src={house} alt="house" />
             </div>
-            <div>
+            <div className="gallery-container">
+                <div className="grid-item">
+                    <div>
+                        <img className="head-image" src={house} alt="house" />
+                    </div>
+                    <div>
+                        <h2>EXTERIOR</h2>
+                    </div>
+
+                </div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
+                <div className="grid-item"></div>
 
             </div>
         </div>
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,14 +2,72 @@ import { useEffect, useState } from 'react';
 import './Header.css';
 
 
+function debounce(fn, ms) {
+    let timer
+    return _ => {
+        clearTimeout(timer)
+        timer = setTimeout(_ => {
+            timer = null
+            fn.apply(this, arguments)
+        }, ms)
+    };
+}
+
+
 function Header({ toggleTab }) {
 
+    const [menu, setMenu] = useState("mobile-menu-hidden");
+
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const debouncedHandleResize = debounce(function handleResize() {
+            setWidth(window.innerWidth)
+        }, 1000)
+
+        window.addEventListener('resize', debouncedHandleResize)
+
+        return _ => {
+            window.removeEventListener('resize', debouncedHandleResize)
+        }
+    })
+
+    const handleMenu = () => {
+        if (menu === "mobile-menu-in") {
+            setMenu("mobile-menu-hidden")
+        } else if (menu === "mobile-menu-hidden") {
+            setMenu("mobile-menu-in")
+        }
+    }
+
     return (
-        <div className="header">
-            <div onClick={() => toggleTab("home")}>
-                <h1>Canedo Builders</h1>
+        <>
+            <div className="header">
+                <div onClick={() => toggleTab("home")}>
+                    <h1>Canedo Builders</h1>
+                </div>
+                <div className="tabs-container">
+
+                    {
+                        width < 550 ? <div onClick={handleMenu}>Button</div> :
+                            <>
+                                <div onClick={() => toggleTab("gallery")}>
+                                    <p>Gallery</p>
+                                </div>
+                                <div onClick={() => toggleTab("current projects")}>
+                                    <p>Current Projects</p>
+                                </div>
+                                <div onClick={() => toggleTab("about")}>
+                                    <p>About</p>
+                                </div>
+                                <div onClick={() => toggleTab("contact")}>
+                                    <p>Contact</p>
+                                </div>
+                            </>
+                    }
+
+                </div>
             </div>
-            <div className="tabs-container">
+            <div className={menu}>
                 <div onClick={() => toggleTab("gallery")}>
                     <p>Gallery</p>
                 </div>
@@ -23,7 +81,7 @@ function Header({ toggleTab }) {
                     <p>Contact</p>
                 </div>
             </div>
-        </div>
+        </>
 
     );
 }
