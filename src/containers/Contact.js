38a88338e64232948252959e9f185975747ebabd
franco-canedo <francanedo1@hotmail.com>
2021-11-14 16:44:02 -0600

    contact phone number added

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -22,6 +22,7 @@ function Contact() {
                     
                     
                     <h3>INFO@CANEDOBUILDERS.COM</h3>
+                    <h3>(512)-373-5885</h3>
                 </div>
                 <div className="form-div">
                     <h3>SEND US A MESSAGE</h3>
