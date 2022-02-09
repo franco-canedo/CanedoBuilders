c57be17908940630f17ac0fcdd2427788e34d018
franco-canedo <francanedo1@hotmail.com>
2022-02-09 14:28:23 -0600

    warranty request form added

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -14,40 +14,53 @@ function Contact() {
     return (
         <div className="gallery-master-container">
             <div></div>
-           
+
             <div className="contact-container">
-            <img src={house} className="img-background"></img>
+                <img src={house} className="img-background"></img>
                 <div className="info-div">
-                   
+
                     <p></p>
-                    
-                    
+
+
                     <h3>INFO@CANEDOBUILDERS.COM</h3>
                     <h3>(512)-888-4112</h3>
                 </div>
                 <div className="form-div">
-                    <h3>SEND US A MESSAGE</h3>
-                    <p></p>
+                    <h2>Warranty Request Form</h2>
+                    <p>Please fill out the form below for warranty related issues.  </p>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>{<Icon name='envelope outline' size='large' />}</Form.Label>
+                            <Form.Label>{<Icon name='angle right' size='large' />} Name </Form.Label>
+                            <Form.Control type="name" placeholder="..." />
+
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>{<Icon name='envelope outline' size='large' />} Email </Form.Label>
                             <Form.Control type="email" placeholder="name@example.com" />
                         </Form.Group>
-                        <Form.Group className="mb-3" 
-                        controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>{<Icon name='pencil alternate' size='large' />}</Form.Label>
-                            <Form.Control as="textarea" rows={3}  placeholder="..."/>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>{<Icon name='phone volume' size='large' />} Phone Number </Form.Label>
+                            <Form.Control type="number" placeholder="..." />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>{<Icon name='certificate' size='large' />} Issue </Form.Label>
+                            <Form.Control type="email" placeholder="..." />
+                        </Form.Group>
+                        <Form.Group className="mb-3"
+                            controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>{<Icon name='pencil alternate' size='large' />} Details</Form.Label>
+                            <Form.Control as="textarea" rows={3} placeholder="..." />
                         </Form.Group>
                         <a href="mailto:info@canedobuilders.com?subject=Subject&body=Body">
-                        <Button variant="contained" color="primary">
-                        
-                             {<Icon name='paper plane' size='large' />}
-                        </Button>
+                            <Button variant="contained" color="primary">
+
+                                {<Icon name='paper plane' size='large' />}
+                            </Button>
                         </a>
                     </Form>
                 </div>
             </div>
-        </div>
+        </div >
 
     );
 }
