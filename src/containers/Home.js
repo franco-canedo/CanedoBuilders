892ccd230afd3cffe2cc6376764b9e06af87d9d5
franco-canedo <francanedo1@hotmail.com>
2022-02-12 17:07:52 -0600

    see our work added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -85,7 +85,7 @@ function Home({ toPage }) {
                
                 <div>
                 <div className="see-work">
-                    <h5>SEE OUR WORK {<Icon name='caret square down' size='large' />}</h5>
+                    <h5>SEE OUR WORK </h5>
                 </div>
                     <video width="750" height="500" controls >
                         <source src="/Videos/bethuneVid.mp4" type="video/mp4" /></video>
